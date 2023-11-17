25e385a84e546396daaed25864c0ef95710821f8
Mark Julien Hahn <mail@markjulienhahn.de>
2023-11-17 15:55:14 +0100

    first commit

---
+++ components/Main.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Main = () => {
+  return (
+    <div className="placeholder">
+      <p>
+        This is <a href="mailto:mail@markjulienhahn.de">Mark Julien Hahn</a>'s
+        official landing page. He is not showing any of his
+        <em>graphic design</em> or <em>programming</em> projects. <br />
+        <br />
+        If you are interested in seeing some of his work, go ahead and visit the
+        website of the type-design label that he co-founded, called <a href="https://stereotypefaces.com">Stereo Typefaces</a>. This is the
+        project that he is currently most passionate about.
+        <br />
+        <br />
+        If there are still some questions left, feel free to write him an <a href="mailto:mail@markjulienhahn.de">E-Mail</a>, he will be happy to
+        reply. ✌️
+      </p>
+    </div>
+  );
+};
+
+export default Main;
